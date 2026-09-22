02d56c1c313ad5afac3a7978a377e210c5b5b8a7
/Maycon Vyctor <mayconsantosfc11@gmail.com>
2026-09-21 23:48:58 -0300

    feat(task): completa os endpoints da classe TaskController

--- src/main/java/com/projetoTodolist/controller/TaskController.java
+++ src/main/java/com/projetoTodolist/controller/TaskController.java
@@ -3,16 +3,23 @@ package com.projetoTodolist.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetoTodolist.model.Task;
 import com.projetoTodolist.service.TaskService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -24,12 +31,33 @@ public class TaskController {
     private TaskService taskService;
 
     @GetMapping
-    public List<Task> getAll() {
-        return taskService.findAllTasks();
+    public ResponseEntity<List<Task>> getAll() {
+        List<Task> tasks = taskService.findAllTasks();
+        return ResponseEntity.ok(tasks);
+    }
+
+    @GetMapping("/{id}")
+        public ResponseEntity<Task> getById(@PathVariable Long id) {
+        return taskService.findById(id)
+                .map(task -> ResponseEntity.ok(task))
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public Task create(@RequestBody Task task) {
-        return taskService.save(task);
+    public ResponseEntity<Task> create(@Valid @RequestBody Task task) {
+        Task taskCriada = taskService.save(task);
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskCriada);
+    }
+
+    @PutMapping("/{id}") 
+    public ResponseEntity<Task> update(@PathVariable Long id, @Valid @RequestBody Task task) {
+        Task updatedTask = taskService.update(id, task);
+        return ResponseEntity.ok(updatedTask);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        taskService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
